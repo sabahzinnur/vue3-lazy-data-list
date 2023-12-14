2c18cf251c3690737210b5c171760b9db077bc48
sabahzinnur <sabahzinnur@gmail.com>
2023-12-14 14:10:00 +0500

    style: fix code style

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import vuePlugin from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
-    plugins: [vuePlugin()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
+  base: './',
+  plugins: [vuePlugin()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    server: {
-        port: 3000,
-    },
+  },
+  server: {
+    port: 3000,
+  },
 })
